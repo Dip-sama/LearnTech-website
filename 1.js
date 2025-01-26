2bd0e90b06886fc4fb1dc5e350df013b0bc0a130
Dip Bhattacharjee <bhattacharjeedip3@gmail.com>
2025-01-26 20:47:49 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,29 +1,16 @@
-(function(doc){
-  var scriptElm = doc.scripts[doc.scripts.length - 1];
-  var warn = ['[ionicons] Deprecated script, please remove: ' + scriptElm.outerHTML];
+// Sidebar Toggle
+function openSidebar() {
+  const navbar = document.getElementById('navbar');
+  const overlay = document.getElementById('overlay');
+  navbar.classList.add('active');
+  overlay.style.display = 'block';
+  document.getElementById('open-sidebar-button').setAttribute('aria-expanded', 'true');
+}
 
-  warn.push('To improve performance it is recommended to set the differential scripts in the head as follows:')
-
-  var parts = scriptElm.src.split('/');
-  parts.pop();
-  parts.push('ionicons');
-  var url = parts.join('/');
-
-  var scriptElm = doc.createElement('script');
-  scriptElm.setAttribute('type', 'module');
-  scriptElm.src = url + '/ionicons.esm.js';
-  warn.push(scriptElm.outerHTML);
-  scriptElm.setAttribute('data-stencil-namespace', 'ionicons');
-  doc.head.appendChild(scriptElm);
-
-  
-  scriptElm = doc.createElement('script');
-  scriptElm.setAttribute('nomodule', '');
-  scriptElm.src = url + '/ionicons.js';
-  warn.push(scriptElm.outerHTML);
-  scriptElm.setAttribute('data-stencil-namespace', 'ionicons');
-  doc.head.appendChild(scriptElm)
-  
-  console.warn(warn.join('\n'));
-
-})(document);
+function closeSidebar() {
+  const navbar = document.getElementById('navbar');
+  const overlay = document.getElementById('overlay');
+  navbar.classList.remove('active');
+  overlay.style.display = 'none';
+  document.getElementById('open-sidebar-button').setAttribute('aria-expanded', 'false');
+}
