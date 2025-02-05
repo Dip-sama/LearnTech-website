269fb5b0fae5fe5cbc202c355bc4f754803baaa8
Dip Bhattacharjee <bhattacharjeedip3@gmail.com>
2025-02-05 23:57:02 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,16 +1,1 @@
-        document.addEventListener("DOMContentLoaded", function () {
-    const hamburger = document.getElementById("hamburger");
-    const navLinks = document.getElementById("nav-links");
-
-    hamburger.addEventListener("click", function () {
-        navLinks.classList.toggle("active");
-        hamburger.classList.toggle("active");
-    });
-
-    document.querySelectorAll(".nav-links a").forEach((link) => {
-        link.addEventListener("click", function () {
-            navLinks.classList.remove("active");
-            hamburger.classList.remove("active");
-        });
-    });
-});
+        
